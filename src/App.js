165affefeb3f89e49b4c06e4a7b5c27c94f65062
myronpereira3 <myronpereira3@gmail.com>
2022-07-22 11:09:03 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Button } from 'react-bootstrap';
+import BasicExample from './Components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      
+     <BasicExample/>
     </div>
   );
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,61 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import { Box, Button, ButtonGroup, Flex } from '@chakra-ui/react'
+import "../style.css"
+import { NavLink } from 'react-bootstrap';
+
+ function BasicExample() {
+  return (
+    <>
+    <Box w='100%'  >
+    <Flex color="white" bgColor="#25cf60" justifyContent="center" alignItems="center"  h="60px">
+        
+Want to become a Reseller, Affiliate, or Integration Partner? We are recruiting for our new Partner Programs.
+<NavLink className='links'  to="https://www.timecamp.com/partners"><u className='underline'> Learn more and apply here ➡️</u></NavLink>
+    </Flex>
+    </Box >
+    {/* Navbar------------------------------------- */}
+    <Box w="100%"   p="1.25rem 1rem">
+        <Box  >
+    <Navbar  expand="lg" className='Navbar'>
+      <Container>
+        <Navbar.Brand style={{marginLeft:"100px"}} href="#home"><img src="https://cdn-m.timecamp.com/img/greenbranding/colorLogo.svg" alt="TimeCamp logo" width="126px" height="31px" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse  >
+          <Nav  style={{marginLeft:"115px"}}>
+            <Nav.Link href="#home">Features</Nav.Link>
+            <Nav.Link href="#link">Pricing</Nav.Link>
+            <Nav.Link href="#link">Integrations</Nav.Link>
+            <Nav.Link href="#link">Blog</Nav.Link>
+            
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Collapse  >
+          <Nav >
+            <Nav.Link style={{paddingRight:"20px"}} href="#home">Book a Demo</Nav.Link>
+           
+            <div style={{border:"0.1px solid #25cf60"}}>
+
+            </div>
+            <Nav.Link style={{paddingLeft:"20px"}} href="#link">Sign in</Nav.Link>
+            
+            
+          </Nav>
+          <Button p="27px 40px "  ml="20px" fontSize="1rem" borderRadius="30px" bgColor="#f7b801" ><Link to="">Go to App</Link></Button>
+        </Navbar.Collapse>
+        
+      </Container>
+    </Navbar>
+    </Box>
+    </Box>
+    </>
+  );
+//   ---------------------herosection================
+<Box>
+  
+</Box>
+}
+
+export default BasicExample;
